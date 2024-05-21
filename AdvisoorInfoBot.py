050c8e4abd928bb4c31e95c08c86818f9347af83
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-21 20:31:38 +0100

    Update AdvisoorInfoBot.py

--- AdvisoorInfoBot.py
+++ AdvisoorInfoBot.py
@@ -15,11 +15,6 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 SOLSCAN_API_KEY = os.getenv('SOLSCAN_API_KEY')
 
-# Debugging: Print environment variables to ensure they are loaded correctly
-print(f"TELEGRAM_TOKEN: {TELEGRAM_TOKEN}")
-print(f"CHAT_ID: {CHAT_ID}")
-print(f"SOLSCAN_API_KEY: {SOLSCAN_API_KEY}")
-
 # Check if the TELEGRAM_TOKEN is set
 if not TELEGRAM_TOKEN:
     raise ValueError("TELEGRAM_TOKEN environment variable is not set")
